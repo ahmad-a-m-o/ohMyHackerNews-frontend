3534e0103c158769e128e45e739e74db67fcce12
iss <iss.siamak@gmail.com>
2021-09-04 18:25:27 +0430

    add main project routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About";
+import Login from "../views/Login";
+import Signup from "../views/Signup";
+import Profile from "../views/Profile"
+import store from "../store";
+
 
 Vue.use(VueRouter);
 
@@ -13,16 +19,46 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: {loginRejected: true},
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+    meta: {loginRejected: true},
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+    meta: {loginRequired: true},
+  }
 ];
 
 const router = new VueRouter({
   routes,
+});
+
+router.beforeEach((to,from,next)=>{
+  if(to.matched.some(r=>r.meta.loginRequired)){
+      if(store.state.isAuthtenticated){
+        next();
+      }else{
+        next('/login');
+      }
+  }else if(to.matched.some(r=>r.meta.loginRejected)){
+      if(store.state.isAuthtenticated){
+        next('/');
+      }else{
+        next();
+      }
+  }else next();
 });
 
 export default router;
