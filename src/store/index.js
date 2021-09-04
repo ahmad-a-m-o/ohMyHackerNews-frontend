b593413ca1e6bc65df2dd96e33c7cba30185401d
iss <iss.siamak@gmail.com>
2021-09-04 18:22:41 +0430

    add auth and token state to store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,28 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    isAuthtenticated: false,
+    token: '', 
+  },
+  mutations: {
+    onStart(state){
+      state.token = localStorage.getItem('token');
+      if( state.token && state.token!='' )
+        state.isAuthtenticated = true;
+      else state.isAuthtenticated = false;
+    },
+    login(state, token){
+      state.isAuthtenticated=true;
+      state.token = token;
+      localStorage.setItem('token', token);
+    }, 
+    logout(state){
+      state.isAuthtenticated=false;
+      state.token = '';
+      localStorage.removeItem('token');
+    },
+  },
   actions: {},
   modules: {},
 });
