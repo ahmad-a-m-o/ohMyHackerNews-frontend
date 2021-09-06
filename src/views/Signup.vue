3d3c1af488c03e4259e67707f6712edaa0187250
iss <iss.siamak@gmail.com>
2021-09-06 19:14:08 +0430

    make sign up page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,9 +1,30 @@
 <template>
-    <h2> Sign up page... </h2>
+<div class="text-center col-md-6 col-lg-3">
+  <form class="form-signin">
+      <h2 class="h3 mb-3 font-weight-normal">Create Account</h2>
+
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus=""> <br/>
+
+      <label for="inputName" class="sr-only">name</label>
+      <input type="email" id="inputName" class="form-control" placeholder="Name" required="" autofocus=""> <br/>
+
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""> <br/>
+
+      <button class="btn btn-primary btn-block" @click="signup">Sign up</button>
+  </form>
+</div>
 </template>
 
 <script>
 export default {
   name: "Signup",
+  methods:{
+    signup: function(){
+      console.log('account created!');
+      this.$router.push('/login');
+    }
+  },
 };
 </script>
