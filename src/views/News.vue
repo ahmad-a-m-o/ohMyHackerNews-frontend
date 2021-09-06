a6a2daeec62775c35ec79195e92ad3e94d93203e
iss <iss.siamak@gmail.com>
2021-09-06 19:14:47 +0430

    in news we will show news

---
+++ src/views/News.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="News">
+    <div class="card-header"> {{this.title}} </div>
+    <div class="card-body">
+        Content: {{this.content}}
+        <blockquote class="blockquote mb-0">
+            <footer class="blockquote-footer">
+                Author: {{this.username}}, Message id: {{this.id}} at {{this.date}}
+            </footer>
+        </blockquote>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "News",
+  data(){
+    return{
+        id: this.$route.params.id,
+        username: " user name from api ",
+        title: ' title from api... ',
+        date: 'aa-bb-cccc dd:ee',
+        content: " to fetch message from api... ",
+    };
+  },
+};
+</script>
