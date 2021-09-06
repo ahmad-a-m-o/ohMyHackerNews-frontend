50f3c9807dbd9a99820a238639c04ef584977f82
iss <iss.siamak@gmail.com>
2021-09-06 19:11:09 +0430

    reading news in home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Home Page" />
+    <div v-for="{id,date,title,author} in this.$store.state.fetchedNewsHeader" :key="id">
+      <NewsHead :id="id" :date="date" :title="title" :author="author"/>
+    </div>
+    <div class="d-flex justify-content-center">
+      <b-button  class="btn-sm btn-block mb-2" variant="outline-secondary" @click="loadMore" v-if="!this.isloading"> more </b-button>
+      <b-spinner class="mb-5" style="width: 3rem; height: 3rem; center" label="Large Spinner" type="grow" v-if="this.isloading"></b-spinner>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import NewsHead from "@/components/NewsHead.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    NewsHead,
   },
+  data(){
+    return {isloading: false, };
+  },
+  methods:{
+    loadMore: function(){
+      this.isloading = true;
+      setTimeout( ()=>{
+          this.$store.commit("addNewsHeder",{id:Math.random(), title:"title of news", author:"noone", date:"12-12-2020 12:12"});
+          this.isloading = false;
+      },1000);
+    },
+  }
 };
 </script>
