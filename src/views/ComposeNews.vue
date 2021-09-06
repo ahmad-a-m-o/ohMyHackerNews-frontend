3ac6c3e17e5bd6e55705fbf59a6ca27888372734
iss <iss.siamak@gmail.com>
2021-09-06 19:12:58 +0430

    add view for writing new news

---
+++ src/views/ComposeNews.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="ComposeNews">
+  <form class="">
+      <h2 class="h3 mb-3 font-weight-normal">Compose new News</h2>
+      <b-input type="text" class="form-control" placeholder="Title" autofocus="" v-model="title"/> <br/>
+      <b-textarea type="text" class="form-control" rows="10" placeholder="Content" autofocus="" v-model="content"/> <br/>
+      <b-button class="btn btn-primary btn-block" @click="send">Send</b-button>
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ComposeNews",
+  data(){
+    return  { 
+        enContent: '',
+    };
+  },
+  computed:{
+      content:{ get() {return this.$store.state.composeData.content}, set(value) {this.$store.commit("updateComposeData_content",value)}, },
+      title:{   get() {return this.$store.state.composeData.title},   set(value) {this.$store.commit("updateComposeData_title",value)},   },
+  },
+  watch:{
+      content: function(){this.enContent = btoa(this.$store.state.composeData.content)},  // create base64 of content
+  },
+  methods:{
+      send: function(){
+          console.log(this.title,this.enContent);
+      },
+  },
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,35 +1,16 @@
 <template>
-<div>
-    <h3>Login page, to compelte...</h3>
-    <div class="col-md-6 col-lg-4">
-    <div class="login-wrap py-5">
-    <div class="img d-flex align-items-center justify-content-center" style="background-image: url(images/bg.jpg);"></div>
-    <h3 class="text-center mb-0">Welcome</h3>
-    <p class="text-center">Sign in by entering the information below</p>
-    <form action="#" class="login-form">
-    <div class="form-group">
-    <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-user"></span></div>
-    <input type="text" class="form-control" placeholder="Username">
-    </div>
-    <div class="form-group">
-    <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-lock"></span></div>
-    <input type="password" class="form-control" placeholder="Password">
-    </div>
-    <div class="form-group d-md-flex">
-    <div class="w-100 text-md-right">
-    <a href="#">Forgot Password</a>
-    </div>
-    </div>
-    <div class="form-group">
-    <button class="btn form-control btn-primary rounded px-3" @click="login">Get Started</button>
-    </div>
-    </form>
-    <div class="w-100 text-center mt-4 text">
-    <p class="mb-0">Don't have an account?</p>
-    <router-link to="/signup">Sign up</router-link>
-    </div>
-    </div>
-    </div>
+<div class="text-center col-md-6 col-lg-3">
+  <form class="form-signin">
+      <h2 class="h3 mb-3 font-weight-normal">Login page</h2>
+
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus=""> <br/>
+
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required=""> <br/>
+
+      <button class="btn btn-primary btn-block" @click="login">Sign in</button>
+  </form>
 </div>
 </template>
 
