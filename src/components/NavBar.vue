a64b92ed15ed3f5d93e93701c9f030daf3a3bfaa
iss <iss.siamak@gmail.com>
2021-09-11 19:20:07 +0430

    update components

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,38 +1,78 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" class="bg-dark">
+  <b-navbar toggleable="lg" type="dark"  variant="info" class="bg-dark p-2">
     <b-navbar-brand>`Oh My` Hacker News</b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-            <router-link to="/"        class="text-secondary nav-link nav-item" exact-active-class="text-light">Home page</router-link>
-            <router-link to="/login"   class="text-secondary nav-link nav-item" exact-active-class="text-light" v-show="!this.isLoggedIn">Login</router-link>
-            <router-link to="/signup"  class="text-secondary nav-link nav-item" exact-active-class="text-light" v-show="!this.isLoggedIn">Sign up</router-link>
-            <router-link to="/compose" class="text-secondary nav-link nav-item" exact-active-class="text-light" v-show="this.isLoggedIn">Compose</router-link>
-            <router-link to="/about"   class="text-secondary nav-link nav-item" exact-active-class="text-light">About us</router-link>
-        </b-navbar-nav>
-        <b-navbar-nav class="ml-auto">
-            <router-link to="/profile" class="text-secondary nav-link nav-item" exact-active-class="text-light" v-show="this.isLoggedIn">Profile</router-link>
-            <b-button @click="logout"  class="btn btn-danger m-1" size="sm" v-if="this.isLoggedIn">Logout</b-button>
-        </b-navbar-nav>
+      <b-navbar-nav>
+        <router-link
+          to="/"
+          class="text-secondary nav-link nav-item"
+          exact-active-class="text-light"
+          >Home page</router-link
+        >
+        <router-link
+          to="/login"
+          class="text-secondary nav-link nav-item"
+          exact-active-class="text-light"
+          v-show="!this.isLoggedIn"
+          >Login</router-link
+        >
+        <router-link
+          to="/signup"
+          class="text-secondary nav-link nav-item"
+          exact-active-class="text-light"
+          v-show="!this.isLoggedIn"
+          >Sign up</router-link
+        >
+        <router-link
+          to="/compose"
+          class="text-secondary nav-link nav-item"
+          exact-active-class="text-light"
+          v-show="this.isLoggedIn"
+          >Compose</router-link
+        >
+        <router-link
+          to="/about"
+          class="text-secondary nav-link nav-item"
+          exact-active-class="text-light"
+          >About us</router-link
+        >
+      </b-navbar-nav>
+
+      <b-navbar-nav style="margin-left:auto">
+        <router-link
+          to="/profile"
+          class="text-secondary nav-link nav-item"
+          exact-active-class="text-light"
+          v-show="this.isLoggedIn"
+          >Profile</router-link
+        >
+        <b-button
+          @click="logout"
+          class="btn btn-danger m-1"
+          size="sm"
+          v-if="this.isLoggedIn"
+          >Logout</b-button
+        >
+      </b-navbar-nav>
     </b-collapse>
   </b-navbar>
 </template>
 
-
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 export default {
-    name: 'NavBar',
-    methods:{
-        logout: function(){
-            this.$store.commit('logout');
-            this.$router.push('/');
-        },
+  name: "NavBar",
+  methods: {
+    logout: function () {
+      this.$store.commit("logout");
+      this.$router.push("/");
     },
-    computed: {
-        ...mapState({
-            isLoggedIn: 'isAuthtenticated'
-        }),
-    },
-}
+  },
+  computed: {
+    ...mapState({
+      isLoggedIn: "isAuthtenticated",
+    }),
+  },
+};
 </script>
--- src/components/NewsHead.vue
+++ src/components/NewsHead.vue
@@ -1,23 +1,31 @@
 <template>
-<div class="card mb-4">
-    <div class="card-header"> Author: {{this.author}} </div>
+  <div class="card mb-4">
+    <div class="card-header">Author: {{ this.author }}</div>
     <div class="card-body">
-        <blockquote class="blockquote mb-0">
-            {{this.title}} <br/>
-            <footer class="blockquote-footer">
-                Message id: {{this.id}} at {{this.date}}
-                <router-link :to="{name: 'news', params: {id: this.id }}" style="position:absolute; right:2%;">read more</router-link>
-            </footer>
-        </blockquote>
+      <blockquote class="blockquote mb-0">
+        {{ this.title }} <br />
+        <em class="blockquote-footer">
+          Message id: {{ this.id }} at {{ this.date }}
+          <router-link
+            :to="{ name: 'news', params: { id: this.id } }"
+            style="position: absolute; right: 2%"
+            >read more</router-link
+          >
+        </em>
+      </blockquote>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "NewsHead",
-    props: {
-        id: Number, author: String, title: String, date: String,
-    },
-}
+  name: "NewsHead",
+  props: {
+    id: String,
+    idC: Number,
+    author: String,
+    title: String,
+    date: String,
+  },
+};
 </script>
