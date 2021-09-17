3681ffa8b788f69060fae4a9796d27dd105381da
iss <iss.siamak@gmail.com>
2021-09-17 17:01:06 +0430

    add more details of user in profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,6 +3,10 @@
     <div class="card-header">this is profie page</div>
     <div class="card-body">
       <blockquote class="blockquote mb-0">
+        Welcome  {{this.$store.state.userInfo.name}}! <br/>
+        User ID: {{this.$store.state.userInfo._id}}   <br/>
+        Email:   {{this.$store.state.userInfo.email}} <br/>
+        ============================= <br />
         your posts are here: <router-link to="/myposts">my Posts</router-link>
         <br />
         ============================= <br />
