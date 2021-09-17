ace00bc969bd1c983f625b7a361eff5dcb65a076
iss <iss.siamak@gmail.com>
2021-09-17 17:19:22 +0430

    create new about us page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,36 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <h1 v-text="chert"></h1>
-    <form class="form-inline">
-      <div class="form-group mx-sm-3 mb-2">
-        <b-input
-          type="number"
-          class="form-control"
-          placeholder="number"
-          v-model="chert"
-        />
-      </div>
-      <b-button class="btn btn-primary mb-2" @click="incChert"
-        >Increase me!</b-button
-      >
-    </form>
-    <b-card> 5 times chert is equal {{ 5 * chert }}</b-card>
+    <h1>Hi, this is my clone of HackerNews website</h1>
+    <p> you can create Account and login and publish news on it! </p>
+    <p> Also, like is available. </p>
+    <p> you can use and change it freely. </p>
+    <b-link href="https://github.com/issFreeProjects/ohMyHackerNews"> GitHub page </b-link>
   </div>
 </template>
 
 <script>
 export default {
   name: "About",
-  data() {
-    return {
-      chert: 0,
-    };
-  },
-  methods: {
-    incChert() {
-      this.chert = Number(this.chert) + 2;
-    },
-  },
 };
 </script>
